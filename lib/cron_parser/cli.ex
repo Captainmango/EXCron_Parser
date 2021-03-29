c0ab397fcdd418f99078b80db9ef912d4b08dd97
Edward <edward.heaver01@gmail.com>
2021-03-29 20:54:11 +0100

    table rex installed

--- lib/cron_parser/cli.ex
+++ lib/cron_parser/cli.ex
@@ -1,11 +1,13 @@
 defmodule CronParser.CLI do
   alias CronParser.Parser, as: Parser
   def main(_args) do
-    IO.puts("Welcome to the CLI")
-    IO.inspect Parser.parse("*", 35)
-    IO.inspect Parser.parse("*", 5)
-    IO.inspect Parser.parse("1,2,3", 5)
-    IO.inspect Parser.parse("1", 10)
+    header = ["test"]
+    rows = [
+    ["test1", Parser.parse("*", 10)]
+    ]
+
+    TableRex.quick_render!(rows, header, "Test")
+    |> IO.puts
 
   end
 
