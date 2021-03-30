37f9bebb25e7c06e11d5acdd524ed2d4381b01e1
Edward <edward.heaver01@gmail.com>
2021-03-30 12:39:47 +0100

    working on cli tests and module

--- lib/cron_parser/cli.ex
+++ lib/cron_parser/cli.ex
@@ -9,6 +9,34 @@ defmodule CronParser.CLI do
     TableRex.quick_render!(rows, header, "Test")
     |> IO.puts
 
+    print_commands
+
+  end
+
+  @commands %{
+    "--quit" => "closes the application",
+    "--cron" => "usage: --cron [cron_expression] [filepath] parses the supplied cron expression",
+    "--write" => "usage: --write [filepath] [filename] writes a file to the supplied location",
+    "--help" => "prints the above list of commands again"
+  }
+
+  defp receive_command do
+    IO.gets("\n ")
+    |> String.trim
+    |> String.downcase
+    |> String.split(" ")
+    |> execute_command
+  end
+
+  defp execute_command("quit") do
+    IO.puts "\n Closed parser"
+  end
+
+  defp print_commands do
+    IO.puts "The app can do the following: \n"
+    @commands
+    |> Enum.map(fn({command, description}) -> IO.puts(" #{command} - #{description} ") end)
+    IO.puts "---"
   end
 
 end
--- test/cli_test.exs
+++ test/cli_test.exs
@@ -1,5 +1,25 @@
 defmodule CLITest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  describe "can handle commands" do
+
+    test "can parse commands from user input" do
+
+    end
+
+    test "reacts to the help command" do
+      assert capture_io(fn -> CronParser.CLI.execute_command("quit") end ) == "\n Closed parser\n"
+    end
+
+    test "can react to cron command" do
+
+    end
+
+    test "can react to the write command" do
+
+    end
+  end
 
 
 end
